8adcef4c67313e8ccc7e8f6a1e55459b4e2c722b
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-09 03:08:20 +0600

    initial commit with starter template

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Cosmos Leather BD',
+    language: 'en',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-typography',
+  ],
+};
